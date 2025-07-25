bd7b11b1a988d36702a1603fbec009e3f7a6a20e
Ayush Sah <ayush.sah@Ayush-Sah-Breaking-Wave-DB-Ltd.local>
2025-07-25 12:13:02 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { AuthProvider } from './context/context';
-import RootLayout from './app/_layout';
-import LoginScreen from './components/login';
+import React from "react";
+import { AuthProvider } from "./context/context";
+import RootLayout from "./app/_layout";
+import LoginScreen from "./components/login";
+import { LogBox } from "react-native";
+LogBox.ignoreLogs([
+  "Warning: Unknown event handler property `onResponderTerminate`",
+]);
 
 const App: React.FC = () => {
-  return (
-      <RootLayout />
-  );
+  return <RootLayout />;
 };
 
 export default App;
